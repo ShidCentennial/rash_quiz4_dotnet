5fada9b1f6aad5c27920c044575a433e3f2becdd
ShidCentennial <rang1@my.centennialcollege.ca>
2025-04-07 12:05:25 -0400

    Initial Commit

---
+++ Quiz4Devops/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quiz4Devops.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
